12b3da8d6ead3bbbc852e017c9bdfeca5b1f7b0c
Arnaud ALLEMAND <a.allemand0@gmail.com>
2021-12-28 15:52:53 +0100

    Images opened using cv2

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect
+import numpy as np
+import cv2
 import os
 
 app = Flask(__name__)
-app.config["IMAGE_UPLOADS"] = "/home/arnaud/Documents/Markhead_FlaskAPI/static/img/uploads"
+app.config["IMAGE_UPLOADS"] = "/home/arnaud/Documents/MarkHead_FlaskAPI/static/img/uploads/"
 
 @app.route("/")
 def index():
@@ -21,3 +23,27 @@ def upload_image():
             return redirect(request.url)
             
     return render_template("upload_image.html", title="Uploading an image")
+
+@app.route("/analyse-image", methods=["GET", "POST"])
+def analyse_image():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            #app.logger.debug("image_name : ", image.filename)
+
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+
+            app.logger.debug("OK save")
+
+            img = cv2.imread(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+
+            app.logger.debug("OK cv2")
+            app.logger.debug("type(img) :", type(img))
+            app.logger.debug("img.shape :", str(img.shape))
+
+            #imagenp = np.fromstring(imagestr, np.uint8)
+
+
+            return redirect(request.url)
+            
+    return render_template("analyse_image.html", title="Analysing an image")
