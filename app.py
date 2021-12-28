1ae039d561804b3c82ea0aee58b97144f89e01d3
Arnaud ALLEMAND <a.allemand0@gmail.com>
2021-12-28 17:26:00 +0100

    Alignment from uploaded picture + saving in a spare folder functionnal

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, redirect
+from deepface import DeepFace
 import numpy as np
 import cv2
 import os
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 app.config["IMAGE_UPLOADS"] = "/home/arnaud/Documents/MarkHead_FlaskAPI/static/img/uploads/"
+app.config["ALIGNED_FACES"] = "/home/arnaud/Documents/MarkHead_FlaskAPI/static/img/aligned_faces/"
+
+#Global variable for DeepFace : every detection algorithms
+backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface']
 
 @app.route("/")
 def index():
@@ -35,14 +41,26 @@ def analyse_image():
 
             app.logger.debug("OK save")
 
-            img = cv2.imread(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            #img = cv2.imread(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
 
-            app.logger.debug("OK cv2")
-            app.logger.debug("type(img) :", type(img))
-            app.logger.debug("img.shape :", str(img.shape))
+            #app.logger.debug("OK cv2")
+            #app.logger.debug("type(img) :", type(img))
+            #app.logger.debug("img.shape :", str(img.shape))
 
             #imagenp = np.fromstring(imagestr, np.uint8)
 
+            try:
+                aligned_face=DeepFace.detectFace(os.path.join(app.config["IMAGE_UPLOADS"], image.filename), detector_backend=backends[1])
+                success=True
+            except ValueError as e:
+                print(e)
+                print("No face found")
+                success=False
+
+            if success:
+                print(type(aligned_face))
+                plt.imsave(os.path.join(app.config["ALIGNED_FACES"], image.filename), aligned_face)
+                #cv2.imwrite(os.path.join(app.config["ALIGNED_FACES"], image.filename), aligned_face)
 
             return redirect(request.url)
             
